e1f49cd03e86f7e31e92e7baff010e0130f4c584
Gazi2050 <gazi45344@gmail.com>
2025-04-25 22:58:08 +0600

    feat:added isEmpty with test-case

---
+++ packsmith/test/utils/check/isEmpty.test.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { isEmpty } from "src";
+import { describe, it, expect } from "vitest";
+
+describe('testing isEmpty', () => {
+    it('handles various types correctly', () => {
+        expect(isEmpty(null)).toBe(true);
+        expect(isEmpty(undefined)).toBe(true);
+        expect(isEmpty('')).toBe(true);
+        expect(isEmpty([])).toBe(true);
+        expect(isEmpty({})).toBe(true);
+        expect(isEmpty('a')).toBe(false);
+        expect(isEmpty([1])).toBe(false);
+        expect(isEmpty({ a: 1 })).toBe(false);
+        expect(isEmpty(0 as any)).toBe(false);
+    });
+});
