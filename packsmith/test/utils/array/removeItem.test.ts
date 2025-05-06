5534abb417e19ad5b09fd7f7c76ef83f7548bce9
Gazi2050 <gazi45344@gmail.com>
2025-05-06 13:46:32 +0600

    test:added test for removeItem

--- packsmith/test/utils/array/removeItem.test.ts
+++ packsmith/test/utils/array/removeItem.test.ts
@@ -6,4 +6,8 @@ describe('removeItem', () => {
         const result1 = { array: [1, 2, 3, 4], message: 'item removed' }
         expect(removeItem(arr, 5)).toEqual(result1)
     })
+    it('return value and "item not found" if item is not exist', () => {
+        const result1 = { array: arr, message: 'item not found' }
+        expect(removeItem(arr, 9)).toEqual(result1)
+    })
 })
