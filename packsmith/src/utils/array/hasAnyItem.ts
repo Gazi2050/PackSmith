c6181d9da1dfc4a72c44089a8d51e366baaa5806
Gazi2050 <gazi45344@gmail.com>
2025-05-07 17:21:12 +0600

    feat:added hasAnyItem

--- packsmith/src/utils/array/hasAnyItem.ts
+++ packsmith/src/utils/array/hasAnyItem.ts
@@ -1,3 +1,8 @@
-export function hasAnyItem() {
-
+export function hasAnyItem(value1: unknown[], value2: unknown[]) {
+    for (const item of value2) {
+        if (value1.includes(item)) {
+            return true;
+        }
+    }
+    return false;
 }
